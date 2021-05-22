b61608e64c450d32794f01d7bfe9b7b8484bf9d4
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-05-22 23:27:53 +0100

    demo data download

---
+++ data/download.sh
@@ -0,0 +1,3 @@
+wget "http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/jaffle_shop_orders.csv"
+wget "http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/jaffle_shop_customers.csv"
+wget "http://dbt-tutorial-public.s3-us-west-2.amazonaws.com/stripe_payments.csv"
